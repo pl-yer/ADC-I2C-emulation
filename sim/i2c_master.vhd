6daf76b560df84542a4c29ecd93593806ea57f6c
stklat <stklat@gmail.com>
2022-01-12 16:04:18 +0100

    start repaired, bus signals modified

--- sim/i2c_master.vhd
+++ sim/i2c_master.vhd
@@ -4,42 +4,45 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity i2c_master is
-    port (
-        reset : in std_logic;
-        --communication with stimulus
-        rdy_for_data : in std_logic;
-        data_rdy : out std_logic;
-        i2c_data : out std_logic_vector(11 downto 0);
-        --i2c communtiation interface        
-        sda : inout std_logic;
-        scl : inout std_logic        
-    );
+  port (
+    reset : in std_logic;
+    --communication with stimulus
+    rdy_for_data : in std_logic;
+    data_rdy : out std_logic;
+    i2c_data : out std_logic_vector(11 downto 0);
+    --i2c communtiation interface        
+    sda_o : out std_logic;
+    scl_o : out std_logic;
+    sda_i : in std_logic;
+    scl_i : in std_logic        
+  );
 end i2c_master;
 
 architecture i2c_master_arch of i2c_master is
 
-    constant CLK_PERIOD_100KHZ : time := 5 ns;
-    type t_RW is (READ, WRITE); 
+  constant CLK_PERIOD_100KHZ : time := 10 us;
+  type t_RW is (READ, WRITE); 
 
-    signal clk : std_logic := '0';
-    signal scl_on : std_logic := '0';  
+  signal clk : std_logic := '0';
+  signal scl_on : std_logic := '0';  
 
 begin
 
     clk <= not clk after CLK_PERIOD_100KHZ/2;
-    scl <= clk when scl_on = '1' else '1'; 
+    scl_o <= clk when scl_on = '1' else '1'; 
     process
         procedure free_bus is
         begin
-            scl <= 'Z';
-            sda <= 'Z';
+            -- scl_o <= '1';
+            sda_o <= '1';
         end procedure;
     
         procedure send_start is
         begin
+            wait until rising_edge(clk);
             scl_on <= '1';
             wait for CLK_PERIOD_100KHZ/4;
-            sda <= '0';
+            sda_o <= '0';
             wait for CLK_PERIOD_100KHZ/4;
         end procedure;
     
@@ -54,36 +57,36 @@ begin
             end if;
             data := slave_addr & rw_sl;
             for i in 7 downto 0 loop
-                wait until falling_edge(scl);
+                wait until falling_edge(scl_i);
                 wait for CLK_PERIOD_100KHZ/4;
-                sda <= data(i);  
+                sda_o <= data(i);  
             end loop;
         end procedure;
 
         procedure read_byte (data_out : out std_logic_vector(7 downto 0)) is
         begin
             for i in 7 downto 0 loop
-                wait until rising_edge(scl);
+                wait until rising_edge(scl_i);
                 wait for CLK_PERIOD_100KHZ/4;
-                data_out(i) := sda;  
+                data_out(i) := sda_i;  
             end loop;
         end procedure;
 
         procedure is_acknowledged(result : out boolean) is
         begin
-            wait until rising_edge(scl);
+            wait until rising_edge(scl_i);
             wait for CLK_PERIOD_100KHZ/4;
-            result := sda = '0';
+            result := sda_i = '0';
         end procedure;
 
         procedure do_acknowledge(result : in boolean) is
         begin
-            wait until falling_edge(scl);
+            wait until falling_edge(scl_i);
             wait for CLK_PERIOD_100KHZ/4;
             if result then
-                sda <= '0';
+                sda_o <= '0';
             else 
-                sda <= '1';
+                sda_o <= '1';
             end if;
         end procedure;
 
@@ -91,10 +94,10 @@ begin
         variable data : std_logic_vector(15 downto 0);
     begin
         free_bus;
-        wait for 10 ns;
-        send_start;
+        wait for 10 ns;   
         while true loop
-            send_address("1010101", READ);
+            send_start;
+            send_address("1001101", READ);
             is_acknowledged(result);
             if result then --else retransmition
                 exit;
--- sim/tb_i2c_master.vhd
+++ sim/tb_i2c_master.vhd
@@ -10,8 +10,8 @@ end tb_i2c_master;
 
 architecture bench of tb_i2c_master is
 
-    signal sda : std_logic;
-    signal scl : std_logic;
+    signal sda_c, sda_o1, sda_o2 : std_logic;
+    signal scl_c, scl_o1, scl_o2 : std_logic;
 
 begin
 
@@ -23,19 +23,27 @@ begin
             data_rdy => open,
             i2c_data => open, 
             --i2c communtiation interface        
-            sda => sda,
-            scl => scl       
+            sda_o => sda_o1,
+            scl_o => scl_o1,
+            sda_i => sda_c,
+            scl_i => scl_c        
         );
 
-    SDA_PULLUP : PULLUP
-    port map (
-    O => sda -- Pullup output (connect directly to top-level port)
-    );
+    sda_c <= sda_o1 and sda_o2;
+    scl_c <= scl_o1 and scl_o2;
+    
+    sda_o2 <= '1';
+    scl_o2 <= '1';
 
-    SCL_PULLUP : PULLUP
-    port map (
-    O => scl -- Pullup output (connect directly to top-level port)
-    );
+    -- SDA_PULLUP : PULLUP
+    -- port map (
+    -- O => sda_c -- Pullup output (connect directly to top-level port)
+    -- );
+
+    -- SCL_PULLUP : PULLUP
+    -- port map (
+    -- O => scl_c -- Pullup output (connect directly to top-level port)
+    -- );
 
 
 end bench;
